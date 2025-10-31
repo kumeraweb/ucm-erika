f5d615487bcbc69be9adc1595faaef48f55d5d2f
Javier Figueroa <javiernfigueroa@gmail.com>
2025-10-30 21:10:04 -0300

    2.0v

--- src/data/site.ts
+++ src/data/site.ts
@@ -2,7 +2,7 @@ export const SITE = {
   name: 'Erika Zuñiga',
   phone: '+56 9 4287 4934',
   city: 'Region Metropolitana',
-  region: '',
+  region: 'V Region',
   email: 'ezuniga@ucmchile.cl',
   slug: '{slug}',
   domain: 'https://tu-dominio.cl'
